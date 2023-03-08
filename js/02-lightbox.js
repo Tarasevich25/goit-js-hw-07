a501be5175014831dc2371a2c7767ac4c34c15b1
Taras Oliynik <tarasoliynik0102@gmail.com>
2023-03-08 09:37:03 +0200

    hw-7

---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const imageList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+    />
+    </a>
+    </div>`;
+  })
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", imageList);
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsType: "attr",
+  captionsData: "alt",
+  captionsDelay: 250,
+});
