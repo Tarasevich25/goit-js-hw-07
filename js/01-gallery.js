52b6252c657194cb4ffd6bf2cf0da8d436b1b4d8
Taras Oliynik <112812514+Tarasevich25@users.noreply.github.com>
2023-04-30 23:10:22 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,53 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-const galleryEl = document.querySelector(".gallery");
-const imgCard = createImgCard(galleryItems);
-
-galleryEl.insertAdjacentHTML("beforeend", imgCard);
-
-galleryEl.addEventListener("click", onImgOriginalCard);
-
-function createImgCard(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-    <div class ="gallery__item">
-    <a class="gallery__link"
-    href ="${original}">
-    <img class="gallery__image"
-    src="${preview}"
-    data-sourse = "${original}"
-    alt = "${description}"
-    width = 100%>
-    </img>
-    </a>
-    </div>`;
-    })
-    .join("");
-}
-
-function onImgOriginalCard(event, preview, description) {
-  event.preventDefault();
-  if (!event.target.classList.contains("gallery__image")) {
-    return;
+const galleryList = document.querySelector(".gallery");
+const galleryItem = galleryItems.map(
+  ({ preview, original, description }) => {
+    return `<li class ="gallery__item">
+    <a class="gallery__link" 
+    href="${original}"> 
+    <img class ="gallery__image" 
+    src="${preview}
+    " data-source="${original}" 
+    alt="${description}
+    " width="340"/></a></li>`;
   }
-
-  function onCloseModal(event) {
+);
+galleryList.insertAdjacentHTML("beforeend", galleryItem.join(""));
+galleryList.addEventListener("click", onImgOriginalCard);
+function onImgOriginalCard(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG"){
+    return
+  }
+  const linkSrc = event.target.dataset.source;
+  const altSrc = event.target.alt;
+  const onCloseModal = function (event) {
     if (event.code === "Escape") {
       instance.close();
     }
-  }
-
+  };
   const markup = `
-<img width="1280 src="${preview}" alt="${description}" >
+<img width="1280" src="${linkSrc}" alt="${altSrc}" >
 `;
-
   const instance = basicLightbox.create(markup, {
     onShow: (instance) => window.addEventListener("keydown", onCloseModal),
     onClose: (instance) => window.removeEventListener("keydown", onCloseModal)
   });
   instance.show();
-
-  // console.log(event.target);
 }
